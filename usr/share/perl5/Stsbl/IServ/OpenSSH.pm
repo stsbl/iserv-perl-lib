d42d5bddcc530616a4b8480b88118f4cae9cbd19
Felix Jacobi <felix.jacobi@stsbl.de>
2017-05-04 23:33:26 +0200

    OpenSSH.pm: Fixed handling of stderr/stdout.

--- usr/share/perl5/Stsbl/IServ/OpenSSH.pm
+++ usr/share/perl5/Stsbl/IServ/OpenSSH.pm
@@ -23,24 +23,28 @@ sub openssh_run($@)
   my @err;
   my $random = new Bytes::Random::Secure();
   my $touch = new File::Touch();
-
+  # Redirect stdderr temporary to prevent annoying warning of ssh :/
+  open OLDSTDERR, ">&STDERR" or die "Couldn't redirect OLDSTDERR: $!";
+  open STDERR, ">/dev/null" or die "Couldn't redirect STDERR: $!";
   my $stdout_file = "/tmp/stsbl-iserv-openssh-".$random->string_from('0123456789', 10);
   my $stderr_file = "/tmp/stsbl-iserv-openssh-".$random->string_from('0123456789', 10);
-  $touch->touch($stdout_file);
-  $touch->touch($stderr_file);
-  # protect *out files from world-wide access
-  chmod 00600, $stdout_file;
-  chmod 00600, $stderr_file;
+  my $known_hosts_file = "/tmp/stsbl-iserv-openssh-".$random->string_from('0123456789', 10);
+  $touch->touch($stdout_file) or die "Couldn't touch file $stdout_file: $!";
+  $touch->touch($stderr_file) or die "Couldn't touch file $stderr_file: $!";
+  $touch->touch($known_hosts_file) or die "Couldn't touch file $known_hosts_file: $!";
+  # protect files from world-wide access
+  chmod 00600, $stdout_file or die "Couldn't chmod file $stdout_file: $!";
+  chmod 00600, $stderr_file or die "Couldn't chmod file $stderr_file: $!";
+  chmod 00600, $known_hosts_file or die "Couldn't chmod file $known_hosts_file: $!";
 
-  open my $stdout_fh, '>>', $stdout_file;
-  open my $stderr_fh, '>>', $stderr_file;
+  open my $stdout_fh, ">>", $stdout_file;
+  open my $stderr_fh, ">>", $stderr_file;
 
   my $ssh = Net::OpenSSH->new(
     $ip,
-    master_stderr_fh => $stderr_fh,
-    master_stdout_fh => $stdout_fh,
+    master_opts => [-i => "/var/lib/iserv/config/id_rsa", -o => "StrictHostKeyChecking=no", -o => "UserKnownHostsFile=$known_hosts_file"],
     user => "root",
-    key_path => "/var/lib/iserv/config/id_rsa"
+    default_stdout_fh => $stdout_fh
   );
 
   if (not $ssh->error)
@@ -48,19 +52,31 @@ sub openssh_run($@)
     $ssh->system(@cmd);
     if ($ssh->error) 
     {
-      push @err, "Could not execute cmd @cmd: $ssh->error";
+      push @err, "Could not execute cmd!";
     }
   } else {
-    push @err, "Could not connect to Port 22: $ssh->error";
-  } 
-  return wantarray ? @err : print STDERR @err;
-  print STDERR @err;
+    push @err, "Could not connect to Port 22!";
+  }
+  # close connection and write output
+  undef $ssh;
   
+  # restore stderr
+  open STDERR, ">&OLDSTDERR" or die "Couldn't redirect STDERR: $!";;
+  close OLDSTDERR or die "Couldn't close OLDSTDERR: $!";;
+
+  if (@err > 0)
+  {
+    print STDERR @err;
+  }
+
   my %ret;
-  $ret{stdout} = IServ::IO::getfile $stdout_file;
-  $ret{stderr} = IServ::IO::getfile $stderr_file;
-  unlink $stdout_file;
-  unlink $stderr_file;
+  $ret{stdout} = getfile $stdout_file;
+  $ret{stderr} = getfile $stderr_file;
+  
+  # clean up
+  unlink $stdout_file or warn "Couldn't unlink file $stdout_file: $!";
+  unlink $stderr_file or warn "Couldn't unlink file $stderr_file: $!";
+  unlink $known_hosts_file or warn "Couldn't unlink file $known_hosts_file: $!";
 
   return %ret;
 }
